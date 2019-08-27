fb32c0a2a119a3ef89d60cb76f7f27237f581c7a
robin <robin@bofink.se>
2019-08-27 10:26:07 +0200

    fix(app): removed deprecated container

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import { GlobalStyle } from './_globalStyle';
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Container>
+      <>
         <Component {...pageProps} />
         <GlobalStyle />
-      </Container>
+      </>
     );
   }
 }
